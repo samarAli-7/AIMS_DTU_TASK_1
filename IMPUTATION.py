fa9586b741576cd8230fbcc383a7646bd7400d8d
samar <samarali.01491625@gmail.com>
2025-10-23 19:57:41 +0530

    Add files via upload

--- IMPUTATION.py
+++ IMPUTATION.py
@@ -31,5 +31,52 @@ for column in df.columns:
     # print(new_column)
     df[column] = new_column
 print()
-print("IMPUTED :-")
+print("IMPUTED(MEAN) :-")
 print(df)
+
+
+
+
+
+
+
+data = {
+    "Physics": [np.nan, 92, 80, np.nan, 85],
+    "Chemistry": [70, 90, np.nan, 88, np.nan],
+    "Maths": [85, np.nan, 78, 90, np.nan]
+}
+df = pd.DataFrame(data)
+
+
+for column in df.columns:
+    sorted_values = []
+    for value in df[column]:
+        if not np.isnan(value):
+            sorted_values.append(value)
+            # print(sorted_values)
+        # else:
+        #     print("didnt found a number")
+    sorted_values.sort()
+
+    # print(sorted_values)
+    n = len(sorted_values)
+
+    if n % 2 == 1:
+        median = sorted_values[n // 2]
+    else:  
+        median = (sorted_values[n//2 - 1] + sorted_values[n//2]) / 2
+    new_column = []
+    for value in df[column]:
+        if np.isnan(value):
+            new_column.append(median)
+        else:
+            new_column.append(value)
+    # print(new_column)
+    df[column] = new_column
+
+print()
+print("IMPUTED(MEDIAN) :-")
+print(df)
+
+
+
